d121ae2de657c2761b77db44f55d8060da26d83d
sthita <sthitapragyan8353@gmail.com>
2026-04-10 16:34:47 +0530

    update login page

--- AdarshWeb/src/pages/loginpage/LoginPage.jsx
+++ AdarshWeb/src/pages/loginpage/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./loginPage.css";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export default function Auth() {
   const navigate = useNavigate();
